9cf4abcec5e854437ffd1c2a57d11faecf02f7f4
Remy Goldschmidt <taktoa@gmail.com>
2016-01-27 13:30:19 -0600

    Remove clever-specific paths from js-client

--- js-client/client.js
+++ js-client/client.js
@@ -6,10 +6,10 @@ subscriber.subscribe(new Buffer(0));
 subscriber.on('message', function(data) {
     console.log(data.toString());
 });
-subscriber.connect("ipc:///home/clever/apps/teamspeak.pub");
+subscriber.connect("ipc://teamspeak.pub");
 
 var req = zmq.socket('req');
-req.connect("ipc:///home/clever/apps/teamspeak.rep");
+req.connect("ipc://teamspeak.rep");
 process.stdin.setEncoding("utf8");
 process.stdin.on("data",function (pkt) {
     req.send(pkt.trim());
